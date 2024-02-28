b1204fc372e8c7756f7a461ead5a9ccdcf9449aa
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-02-28 12:49:12 -0600

    Create structure for column that will contain the selected songs

---
+++ src/Playlist/mockDataPlst.tsx
@@ -0,0 +1,36 @@
+const SONGS = [
+    {
+        name: "Waiting for a girl like you",
+        artist: "Foreigner"
+    },
+    {
+        name: "I dont want to miss a thing",
+        artist: "Aerosmith"
+    },
+    {
+        name: "Summer nights",
+        artist: "John Travolta"
+    },
+    {
+        name: "Come and get your love",
+        artist: "Redbone"
+    },
+    {
+        name: "Hopelessly devoted to you", 
+        artist: "Olivia newton John"
+    },
+    {
+        name: "I dont want to miss a thing",
+        artist: "Aerosmith"
+    },
+    {
+        name: "Summer nights",
+        artist: "John Travolta"
+    },
+    {
+        name: "Come and get your love",
+        artist: "Redbone"
+    }
+];
+
+export default SONGS;
