02cc2c6620a5902629e0fd273928dba976fede1b
Joel Alejandro Milla Lopez <jalejandro.milla@outlook.com>
2024-02-29 07:58:27 -0600

    Solve problem of margin right of row.

--- src/Title/Title.tsx
+++ src/Title/Title.tsx
@@ -3,8 +3,8 @@ import styles from './Title.module.css'
 
 function Title() {
     return (
-        <div className={`row ${styles.container_title}`}>
-            <h1 className={`title text-center ${styles.title}`}>
+        <div className={styles.container_title}>
+            <h1 className={styles.title}>
                 Ja<span className={styles.highlight}>mmm</span>ing
             </h1>
         </div>
